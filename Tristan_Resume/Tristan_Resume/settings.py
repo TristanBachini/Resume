366b29c3acbfde9990d5d0ef5170bcb1b1bb1f11
Tristan Paul Bachini <bachinitristanpaul@gmail.com>
2023-08-24 00:57:10 +0800

    allowed hosts set to everyone

--- Tristan_Resume/Tristan_Resume/settings.py
+++ Tristan_Resume/Tristan_Resume/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n=o@7dx2#sxpp$zi@mdmo%3h#0hxrzu*g$-3wj0a#^h8$t%i-i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
